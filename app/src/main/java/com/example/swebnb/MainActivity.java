4b2e6341eeec59ed89f0fa10898819d69b2cd21b
Sisi <lsisi@student.chalmers.se>
2021-06-24 13:55:01 +0200

    SearchView

--- app/src/main/java/com/example/swebnb/MainActivity.java
+++ app/src/main/java/com/example/swebnb/MainActivity.java
@@ -1,28 +1,39 @@
 package com.example.swebnb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Initialize the bottom navigation view
-        //create bottom navigation view object
+//        Initialize the bottom navigation view
+//        create bottom navigation view object
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,R.id.navigation_communicate)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_communicate)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
--- app/src/main/java/com/example/swebnb/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/swebnb/ui/home/HomeFragment.java
@@ -2,24 +2,47 @@ package com.example.swebnb.ui.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.app.SearchManager;
+import android.widget.SearchView.OnQueryTextListener;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.swebnb.MainActivity;
 import com.example.swebnb.R;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
+    // List View object
+    ListView listView;
+
+    // Define array adapter for ListView
+    ArrayAdapter<String> adapter;
+
+    // Define array List for List View data
+    ArrayList<String> mylist;
 
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -30,6 +53,88 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+       // initialise ListView with id
+        listView = root.findViewById(R.id.listView);
+
+        // Add items to Array List
+        mylist = new ArrayList<>();
+        mylist.add("Kej");
+        mylist.add("GestHouse");
+        mylist.add("kyb");
+        // Set adapter to ListView
+        adapter
+                = new ArrayAdapter<String>(getContext().getApplicationContext(), android.R.layout.simple_list_item_1, mylist);
+        listView.setAdapter(adapter);
+
         return root;
     }
+
+//    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        // Inflate menu with items using MenuInflator
+//        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.search_box_menu, menu);
+
+        // Initialise menu item search bar
+        // with id and take its object
+        MenuItem searchViewItem
+                = menu.findItem(R.id.search_bar);
+        SearchView searchView
+                = (SearchView) MenuItemCompat.getActionView(searchViewItem);
+
+        // attach setOnQueryTextListener
+        // to search view defined above
+        searchView.setOnQueryTextListener(
+                new SearchView.OnQueryTextListener() {
+
+// Override onQueryTextSubmit method
+// which is call
+// when submitquery is searched
+
+                    @Override
+                    public boolean onQueryTextSubmit(String query)
+                    {
+                        // If the list contains the search query
+                        // than filter the adapter
+                        // using the filter method
+                        // with the query as its argument
+                        ArrayList list = new ArrayList<>();
+                        if (list.contains(query)) {
+                            adapter.getFilter().filter(query);
+                        }
+                        else {
+                            // Search query not found in List View
+                            Toast
+                                    .makeText(getContext().getApplicationContext(),
+                                            "Not found",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                        return false;
+                    }
+
+                    // This method is overridden to filter
+// the adapter according to a search query
+// when the user is typing search
+                    @Override
+                    public boolean onQueryTextChange(String newText)
+                    {
+                        adapter.getFilter().filter(newText);
+                        return false;
+                    }
+                });
+
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+
+
 }
+
+
+
+
+
+
